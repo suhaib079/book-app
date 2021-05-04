411f5d5043ed6d502255537c2c27ddba7d94de73
suhaib079 <suhaibabdennabi33@gmail.com>
2021-05-04 23:12:25 +0300

    k

--- public/app.js
+++ public/app.js
@@ -1,4 +1,3 @@
-'use strict'
 'use strict';
 
 $('.topnav').on('click', ()=>{
@@ -9,4 +8,15 @@ $('#searchNav').on('click', ()=>{
     $('.form').toggle()
     $('#topForm').toggle()
     $('.toggle').toggle()
+})
+$('#updateForm').hide()
+$('#updateBtn').on('click', () => {
+    $('#updateForm').toggle();
+    $('.card').toggle()
+    $('#deleteBtn').toggle();
+})
+$('#closeBtn').on('click', () => {
+    $('.card').toggle()
+    $('#deleteBtn').toggle();
+    $('#updateForm').toggle();
 })
